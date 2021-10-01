0c75630deb03637fea377cab0f3baf6382e7d6c5
league <league@imac-22.attlocal.net>
2021-09-30 18:30:12 -0700

    sending heartbeat and recognizes messages

--- src/discordBot.java
+++ src/discordBot.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -15,6 +17,7 @@ import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.net.ssl.HttpsURLConnection;
 import javax.swing.JFileChooser;
+import javax.swing.Timer;
 
 import com.neovisionaries.ws.client.WebSocket;
 import com.neovisionaries.ws.client.WebSocketAdapter;
@@ -24,42 +27,41 @@ import com.neovisionaries.ws.client.WebSocketFrame;
 import com.neovisionaries.ws.client.WebSocketListener;
 import com.neovisionaries.ws.client.WebSocketState;
 
-public class discordBot extends WebSocketAdapter {
+public class discordBot extends WebSocketAdapter implements ActionListener {
 	String channelID;
 	String token;
-
-	public discordBot(String c, String t)  {
+	String name;
+	WebSocket web;
+	int heartbeatInterval;
+	Timer hbTimer;
+	public discordBot(String c, String t, String n) {
 		this.channelID = c;
 		this.token = t;
+		this.name = n;
 		WebSocketFactory wsf = new WebSocketFactory();
 		try {
-			WebSocket web = wsf.createSocket(getGateway());
+			web = wsf.createSocket(getGateway());
 			web.addListener(this);
-			try {
-				web.connect();
-			}
-			catch(WebSocketException e){
-				
-			}
-			try {
-				Thread.sleep(10000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			web.disconnect();
+
+			web.connect();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+		}
+
+		catch (WebSocketException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
-		String channelID = "890750425744171111";
+
 		BufferedReader br = new BufferedReader(new FileReader("/Users/league/Desktop/Token.txt"));
+		String name = br.readLine();
+		String channelID = br.readLine();
 		String token = br.readLine();
 		br.close();
-		new discordBot(channelID, token);
+		new discordBot(channelID, token, name);
 	}
 
 	public void botMessage(String input) {
@@ -105,8 +107,9 @@ public class discordBot extends WebSocketAdapter {
 				data += (char) v;
 				v = in.read();
 			}
+			System.out.println(data);
 			JsonObject o = getJsonObjectFromString(data);
-			return o.getString("url");
+			ans = o.getString("url");
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -114,7 +117,7 @@ public class discordBot extends WebSocketAdapter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return null;
+		return ans;
 
 	}
 
@@ -130,21 +133,70 @@ public class discordBot extends WebSocketAdapter {
 	public void onConnected(WebSocket websocket, Map<String, List<String>> headers) {
 		System.out.println("connected");
 	}
+
 	@Override
 	public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame,
 			boolean closedByServer) {
-	System.out.println("disconnected");
+		System.out.println(clientCloseFrame);
+		System.out.println(serverCloseFrame);
+		System.out.println("disconnected");
 	}
+
 	@Override
 	public void onError(WebSocket websocket, WebSocketException cause) {
 		System.out.println(cause);
 	}
-	@Override
-	public void onTextMessage(WebSocket websocket, String text) {
-	System.out.println(text);
-	}
+
 	@Override
 	public void onFrame(WebSocket websocket, WebSocketFrame frame) {
 		System.out.println(frame);
+		String payload = frame.getPayloadText();
+		JsonObject obj = getJsonObjectFromString(payload);
+		int op = obj.getInt("op");
+		if (op == 10) {
+			JsonObject d = obj.getJsonObject("d");
+			heartbeatInterval = d.getInt("heartbeat_interval");
+			
+			String auth = "{\n\r" + "  \"op\": 2,\n\r" + "  \"d\": {\n\r" + "    \"token\": \"" + token + "\",\n\r"
+					+ "    \"intents\": 513,\n\r" + "    \"properties\": {\n\r" + "      \"$os\": \"linux\",\n\r"
+					+ "      \"$browser\": \"my_library\",\n\r" + "      \"$device\": \"my_library\"\n\r" + "    }\n\r"
+					+ "  }\n\r" + "}\n\r" + "";
+			web.sendText(auth);
+		}
+		else if(op==0) {
+			String type = obj.getString("t");
+			if(type.equals("MESSAGE_CREATE")) {
+				JsonObject d = obj.getJsonObject("d");
+				JsonObject author = d.getJsonObject("author");
+				String message = d.getString("content");
+				String user =author.getString("username");
+				if(!user.equals(name)) {
+					messageReceived(message, user);
+				}
+			}
+			else if(type.equals("READY")) {
+				hbTimer = new Timer(heartbeatInterval, this);
+				sendHeartbeat();
+				hbTimer.start();
+			}
+		}
+	}
+	void messageReceived(String message, String user) {
+		if(message.equals("apple")) {
+			botMessage(user);
+		}
+	}
+	void sendHeartbeat() {
+		String s = "{\n" + 
+				"    \"op\": 1,\n" + 
+				"    \"d\": null\n" + 
+				"}";
+		web.sendText(s);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		sendHeartbeat();
 	}
 }
